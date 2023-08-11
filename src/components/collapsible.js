238264b1787b620746ca7add27c33ea29990ebf9
iamravenous <franco.moya@canid.io>
2023-08-11 14:35:15 -0400

    feat: improve collapsible adaptability and animation

--- src/components/collapsible.js
+++ src/components/collapsible.js
@@ -1,7 +1,7 @@
 const Collapsible = (options) => {
   const defaultOptions = {
-    triggerSelector: "[data-collapsible-trigger]",
-    contentSelector: "[data-collapsible-content]",
+    triggerSelector: "data-collapsible-trigger",
+    contentSelector: "data-collapsible-content",
     activeClass: "is-active",
     visibleClass: "is-visible",
   };
@@ -9,24 +9,77 @@ const Collapsible = (options) => {
   const settings = { ...defaultOptions, ...options };
 
   const init = () => {
-    const triggerElements = document.querySelectorAll(settings.triggerSelector);
+    const triggerElements = document.querySelectorAll(`[${settings.triggerSelector}]`);
     triggerElements.forEach((trigger) => {
       trigger.addEventListener("click", () => {
-        const content = trigger.nextElementSibling;
-        toggleCollapsible(trigger, content);
+        const collapsibleIdentifier = trigger.getAttribute(settings.triggerSelector);
+        const contentElements = document.querySelectorAll(
+          `[${settings.contentSelector}="${collapsibleIdentifier}"]`
+        );
+        toggleCollapsible(trigger, contentElements);
       });
     });
   };
 
-  const toggleCollapsible = (triggerElement, contentElement) => {
-    if (!contentElement) return;
+  const toggleCollapsible = (triggerElement, contentElements) => {
+    if (!contentElements) return;
 
-    if (contentElement.classList.contains(settings.visibleClass)) {
-      contentElement.classList.remove(settings.visibleClass);
-      triggerElement.classList.remove(settings.activeClass);
+    contentElements.forEach((contentElement) => {
+      if (contentElement.classList.contains(settings.visibleClass)) {
+        contentElement.classList.remove(settings.visibleClass);
+        triggerElement.classList.remove(settings.activeClass);
+        toggleContent(contentElement);
+      } else {
+        contentElement.classList.add(settings.visibleClass);
+        triggerElement.classList.add(settings.activeClass);
+        toggleContent(contentElement);
+      }
+    });
+  };
+
+  const toggleContent = (el) => {
+    if (!el) {
+      console.error("The element with the provided identifier doesn't exist.");
+      return;
+    }
+
+    let contentHeight;
+    const isHidden = el.style.display === "none" || el.style.display === "";
+
+    if (isHidden) {
+      el.style.display = "block";
+      el.style.opacity = "1";
+
+      contentHeight = el.scrollHeight;
+
+      el.style.maxHeight = contentHeight + "px";
+
+      el.addEventListener(
+        "transitionend",
+        function onTransitionEnd() {
+          el.style.maxHeight = "fit-content";
+          el.removeEventListener("transitionend", onTransitionEnd);
+        },
+        { once: true }
+      );
     } else {
-      contentElement.classList.add(settings.visibleClass);
-      triggerElement.classList.add(settings.activeClass);
+      contentHeight = el.scrollHeight;
+      el.style.maxHeight = contentHeight + "px";
+      el.offsetHeight;
+
+      el.style.maxHeight = "0";
+      el.style.opacity = "0";
+
+      el.addEventListener(
+        "transitionend",
+        function onTransitionEnd() {
+          el.style.display = "none";
+          el.style.maxHeight = "0";
+          el.style.opacity = "0";
+          el.removeEventListener("transitionend", onTransitionEnd);
+        },
+        { once: true }
+      );
     }
   };
 
